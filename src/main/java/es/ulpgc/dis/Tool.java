5eb37936940ddb3b5b5f4e94f90d18f42e01cd82
GabrielBR16 <gabrieldiques@gmail.com>
2020-12-17 12:41:33 +0000

    I add the new annotation Tool class.

---
+++ src/main/java/es/ulpgc/dis/Tool.java
@@ -0,0 +1,12 @@
+package es.ulpgc.dis;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Tool {
+    boolean aislante();
+}
