cbf05104da9ffbeb9ca3fed087240c7af404735e
GabrielBR16 <gabrieldiques@gmail.com>
2020-12-17 12:55:19 +0000

    I add the objects with its annotations to the inventory.

--- src/main/java/es/ulpgc/dis/Inventory.java
+++ src/main/java/es/ulpgc/dis/Inventory.java
@@ -2,4 +2,16 @@ package es.ulpgc.dis;
 
 public class Inventory {
 
+    @ScrewFactory(material = "steel", weight = 4)
+    private int tirafondo;
+    @ScrewFactory(material = "iron", weight = 6)
+    private int screws;
+    @Tool(insulation = true)
+    private int screwdrivers;
+
+    public Inventory(int tirafondo, int screws, int screwdrivers) {
+        this.tirafondo = tirafondo;
+        this.screws = screws;
+        this.screwdrivers = screwdrivers;
+    }
 }
