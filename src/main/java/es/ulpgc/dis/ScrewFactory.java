d2f75f8bfeecfdf1909c8c5deda4c31671b026e4
GabrielBR16 <gabrieldiques@gmail.com>
2020-12-17 12:48:16 +0000

    I add the new annotation ScrewFactory class. I translate the "aislante" word.

--- src/main/java/es/ulpgc/dis/ScrewFactory.java
+++ src/main/java/es/ulpgc/dis/ScrewFactory.java
@@ -7,6 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface Tool {
-    boolean insulation();
+public @interface ScrewFactory {
+    String material();
+    int weight();
 }
